d1dc467fae49e340a368bc08fc92d8dd91d15c8a
Nihar Patil <nihar.patil98@gmail.com>
2017-07-06 21:23:32 -0700

    wip

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const path = require('path')
 
 io.on('connection', function(socket){
   console.log('socket has connected')
 });
+
+
+app.get('/', function(req,res) { 
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 const port = process.env.PORT || 3000;
 server.listen(port, function(){
